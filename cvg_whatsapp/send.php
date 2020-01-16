a7bb58cbd39f25108eb6b3c131fad99ed2039861
ViniciusOML <limavinicius1@live.com>
2020-01-16 18:00:13 -0300

    atualização de arquivo php, apresenta bug ao enviar email

--- cvg_whatsapp/send.php
+++ cvg_whatsapp/send.php
@@ -7,88 +7,77 @@ $telefone = $_POST['telefone'];
 $empresa =$_POST ['empresa'];
 $solucao = $_POST['solucao'];
 # FIM DAS INFORMAÇÕES DO CLIENTE   
- 
-// Inclui o arquivo class.phpmailer.php localizado na mesma pasta do arquivo php 
-include "PHPMailer-master/PHPMailerAutoload.php"; 
- 
-// Inicia a classe PHPMailer 
-$mail = new PHPMailer(); 
- 
-// Método de envio 
-$mail->IsSMTP(); 
- 
-// Enviar por SMTP 
-$mail->Host = "smtp.kinghost.net"; 
+
+# INICIO: Informações do remetente
+$senha = 'Cvg340397';
+$nomeRemetente = 'Converge Marketing';
+$emailRemetente = 'convergemkt01@gmail.com';
+$titulo = 'Teste de envio PHPMAILER';
+# FIM: Informações do remetente
+
+#DESTINATARIO
+$emailDestinatario = 'vinicius.lima@convergemarketing.com.br';
+
+#MENSAGEM
+
+// Caminho da biblioteca PHPMailer
+require 'PHPMailer-master/PHPMailerAutoload.php';
+
+// Instância do objeto PHPMailer
+$mail = new PHPMailer;
+
+// Configura para envio de e-mails usando SMTP
+$mail->isSMTP();
+
+// Servidor SMTP
+$mail->Host = 'smtp.gmail.com';
+
+// Usar autenticação SMTP
+$mail->SMTPAuth = true;
+
+// Usuário da conta
+$mail->Username = $emailRemetente;
+
+// Senha da conta
+$mail->Password = $senha;
 
 // Tipo de encriptação que será usado na conexão SMTP
-//$mail->SMTPSecure = 'ssl';
- 
-// Você pode alterar este parametro para o endereço de SMTP do seu provedor 
-$mail->Port = 587; 
- 
- 
-// Usar autenticação SMTP (obrigatório) 
-$mail->SMTPAuth = true; 
- 
-// Usuário do servidor SMTP (endereço de email) 
-// obs: Use a mesma senha da sua conta de email 
-$mail->Username = 'vinicius.lima@convergemarketing.com.br'; 
-$mail->Password = 'vini8810'; 
- 
-// Configurações de compatibilidade para autenticação em TLS 
-$mail->SMTPOptions = array( 'ssl' => array( 'verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true ) ); 
- 
-// Você pode habilitar esta opção caso tenha problemas. Assim pode identificar mensagens de erro. 
- //$mail->SMTPDebug = 2; 
- 
-// Define o remetente 
-// Seu e-mail 
-$mail->From = "vinicius.lima@convergemarketing.com.br"; 
- 
-// Seu nome 
-$mail->FromName = "Site Whatsapp"; 
- 
-// Define o(s) destinatário(s) 
-$mail->AddAddress('vinicius.lima@convergemarketing.com.br'); 
- 
-// Opcional: mais de um destinatário
-// $mail->AddAddress('fernando@email.com'); 
- 
-// Opcionais: CC e BCC
-// $mail->AddCC('joana@provedor.com', 'Joana'); 
-// $mail->AddBCC('roberto@gmail.com', 'Roberto'); 
- 
-// Definir se o e-mail é em formato HTML ou texto plano 
-// Formato HTML . Use "false" para enviar em formato texto simples ou "true" para HTML.
-$mail->IsHTML(true); 
- 
-// Charset (opcional) 
-$mail->CharSet = 'UTF-8'; 
- 
-// Assunto da mensagem 
-$mail->Subject = "Leads CVG Solutions"; 
- 
-// Corpo do email 
-$mail->Body = "Olá Rogério, segue lead coletado no site do <strong>CVG Solution:</strong><br>
-                <br>Nome: $nome <br> 
-                <br>Telefone: $telefone<br>
-                <br>Empresa: $empresa <br>
-                <br>E-mail: $email <br>
-                <br>Solução de interesse: $solucao";
- 
-// Opcional: Anexos 
-// $mail->AddAttachment("/home/usuario/public_html/documento.pdf", "documento.pdf"); 
- 
-// Envia o e-mail 
-$enviado = $mail->Send(); 
- 
-// Exibe uma mensagem de resultado 
-if ($enviado) 
-{ 
-    Header( 'Location: http://localhost/webProjects/cvg_whatsapp/' );
-} else { 
-    echo "Houve um erro ao enviar o formulário: ".$mail->ErrorInfo; 
-} 
- 
- 
-?>
+$mail->SMTPSecure = 'ssl';
+
+// Porta do servidor SMTP
+$mail->Port = 465;
+
+// Informa se vamos enviar mensagens usando HTML
+$mail->IsHTML(true);
+
+// Email do Remetente(tem que ser o mesmo usado no usuario da conta)
+$mail->From = $emailRemetente;
+
+// Nome do Remetente
+$mail->FromName = $nomeRemetente;
+
+// Endereço do e-mail do destinatário
+$mail->addAddress($emailDestinatario);
+
+// Assunto do e-mail
+$mail->Subject = $titulo;
+
+// Mensagem que vai no corpo do e-mail
+$mensagem = ' Olá Rogério, segue líder coletado no site do <strong> Solução CVG: </strong> <br>
+<br> Nome: $ nome <br>
+<br> Telefone: $ telefone <br>
+<br> Empresa: $ empresa <br>
+<br> E-mail: $ email <br>
+<br> Solução de interesse: $ solucao ';
+
+$mail->Body = $mensagem;
+
+$mail->SMTPDebug  = 2;
+echo '<pre>';
+var_dump($mail->Send());
+// Envia o e-mail e captura o sucesso ou erro
+if ($mail->Send()) {
+    $enviado = true;
+} else {
+    $enviado = false;
+}
